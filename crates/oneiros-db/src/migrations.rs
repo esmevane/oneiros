77eb94abd8f66453a3426194d5814d9c677b907f
Joseph McCormick <esmevane@gmail.com>
2026-02-13 21:36:20 -0600

    fix: Move migrations to crates.
    Looks like include_str has some issues with files outside of the crates
    directory? We'll see!

--- crates/oneiros-db/src/migrations.rs
+++ crates/oneiros-db/src/migrations.rs
@@ -1,2 +1,2 @@
-pub(crate) const SYSTEM: &str = include_str!("../../../migrations/system.sql");
-pub(crate) const BRAIN: &str = include_str!("../../../migrations/brain.sql");
+pub(crate) const SYSTEM: &str = include_str!("../migrations/system.sql");
+pub(crate) const BRAIN: &str = include_str!("../migrations/brain.sql");
